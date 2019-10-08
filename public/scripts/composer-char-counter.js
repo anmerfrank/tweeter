55fd5ef6fa8bd0886b7515f46c93c66118c4d829
Anne Franklin <anmerfrank@gmail.com>
2019-10-08 21:58:31 +0000

    Added counter functionality

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+  $('.input').keyup('keyup', function () {
+    const text = $(this).val();
+    const charCount = text.length;
+    const remainingChars = 140-charCount;
+    const currentCount = $(this).parent().children('.footer').children('.counter').text(remainingChars)
+
+    console.log(currentCount);
+
+    if (remainingChars < 0) {
+      this.color = red;
+    }
+  });
+
+  });
+
+
+
+// $('.container'). // count down in this class.
+
+// TO MAKE THE TWEETS In a top-down list: 
+
+// $(document).ready(() = {
+// $('#my-button').click(() => {
+// const text = $('#my-input').val();
+// const listItem = $("<li>").text(text);
+// $("#list-container").prepend($listItem);
+// ));
+// });
