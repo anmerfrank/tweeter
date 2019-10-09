8f432a308768d37505436e8e1c8362977e32e19f
Anne Franklin <anmerfrank@gmail.com>
2019-10-09 01:36:59 +0000

    Passed code review; on to adding tweets

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -9,7 +9,7 @@ $(document).ready(function() {
       document.getElementById("counter").style.color = "magenta";
     }
     if (remainingChars > 0) {
-      document.getElementById("counter").style.color = "4d014d";
+      document.getElementById("counter").style.color = "#4d014d";
     }
   });
 
