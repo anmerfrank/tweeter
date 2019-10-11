4197623dc49f7dcf977d548d01137dcd58556a30
Anne Franklin <anmerfrank@gmail.com>
2019-10-11 01:45:25 +0000

    Fixed bug with  helper functions

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,16 +4,11 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-// IMPORTED FUNCTIONS
-
-
 
 $(document).ready(() => {
 
 
-
-
-  // NAVIGATION DROP-BOX CODE
+  // TOGGLE "COMPOSE" BOX
 
   $('#navbutton').on("click", function () {
     $(".new-tweet").slideToggle({
@@ -21,67 +16,16 @@ $(document).ready(() => {
     $(".input").focus();
   })
 
-  // TWEET CREATION
-
-
-  // HELPER FUNCTIONS
-
   const data = [];
 
-  // const loadTweets = function (data) {
-  //   $.ajax({ url: '/tweets', method: 'GET' })
-  //     .then(function (res) {
-  //       renderTweets(res);
-  //     })
-  // };
-
- 
-  
-  // const renderTweets = function (tweets) {
-  //   $('.tweet-body').empty();
-  //   for (let tweet of tweets) {
-  //     let output = createTweetElement(tweet);
-  //     $(`.tweetContainer`).prepend(output);
-  //   }
-  // }
-
-  // const escape = function (str) {
-  //   let div = document.createElement('div');
-  //   div.appendChild(document.createTextNode(str));
-  //   return div.innerHTML;
-  // }
-
-  // const createTweetElement = function (tweet) {
-  //   let timeStart = Date.now();
-  //   let millis = Date.now() - tweet.created_at;
-  //   let dateStamp = (millis / (60 * 60 * 24 * 1000))
-  //   let totalDate = Math.round(dateStamp);
-
-  //   const tweetTemplate = (
-  //     `<article class="tweet-body">
-  //        <header class="tweetheader">
-  //         <span id="usericon"><img src="${tweet.user.avatars}"> ${tweet.user.name} </span><span class="username">${tweet.user.handle}</span>
-  //       </header>
-  //       ${escape(tweet.content.text)}
-  //       <footer class="tweetfooter"><h8>${totalDate} days ago</h8><div class="icons"><i class="fab fa-font-awesome-flag"></i> <i class="fas fa-retweet"></i> <i class="fas fa-heart"></i></div></footer>
-  //     </article>`
-  //   )
-  //   return tweetTemplate;
-  // }
-
-  // ON PAGELOAD
   
   loadTweets();
 
-  // TO POST A TWEET
-
-
+  // POSTING A TWEET
   
     const $form = $('.new-tweet form');
     $form.on('submit', function () {
       event.preventDefault();
-
-      
 
       // ERROR CHECKING - CHARACTER COUNT
 
